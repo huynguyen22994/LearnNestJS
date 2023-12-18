4cf3b75ab1487530ac6ffc0caf10540ea27d3f59
Huy Nguyen <huynguyen.cyberlogitec@gmail.com>
2023-12-18 10:17:05 +0700

    test

--- packages/learn-nest/src/main.ts
+++ packages/learn-nest/src/main.ts
@@ -7,7 +7,7 @@ import { LoggingGlobalInterceptor } from './interceptor/logger-global.intercepto
 
 async function bootstrap() {
   const bug = '22224'
-  const abc = '222223w32'
+  const abc = '222223w322'
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter()); // use exception for global app
 
